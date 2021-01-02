761a384023a6834720b4d9188b543e2a8b8b2aa2
Andres Aldana <batroid@gmail.com>
2021-01-02 20:32:33 -0300

    Update ejecucion.groovy

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -1,42 +1,54 @@
 def call(){
-    pipeline {
-        agent any
-        parameters { choice(name: 'HERRAMIENTA', choices: ['gradle', 'maven'], description: 'opcion de compilacion')
-                   ,string(name: 'stage' , defaultValue: '', description: '')
-                   }
-        
-        stages {
-            stage('Pipeline') {
-                steps {
-                    script {
-                    //segun el valor del parametro se debe llamar a gradle o maven
-                    env.TAREA = ''
-                    echo "HERRAMIENTA SELECCIONADA: ${params.HERRAMIENTA}"   
-                    echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"                             
-                    if (params.HERRAMIENTA == 'gradle'){
-                        	gradle.call()
-                    } else {
-    	                    maven.call()                    
-                    }
+pipeline {
+    agent any
+    parameters { 
+        choice(name: 'HERRAMIENTA', choices: ['gradle', 'maven'], description: 'opcion de compilacion')
+        string(name: 'stage' , defaultValue: '', description: '')
+    }
+    
+    stages {
+        stage('Pipeline') {
+            steps {
+                script {
+                //segun el valor del parametro se debe llamar a gradle o maven
+                env.TAREA = ''
+                echo "1.-HERRAMIENTA SELECCIONADA: ${params.HERRAMIENTA}" 
+                echo "2.-PARAMETROS SELECCIONADOS: ${stage}"   
+                echo "3.-Running ${env.BUILD_ID} on ${env.JENKINS_URL}"   
+                echo "4.-Rama ${env.BRANCH_NAME}" 
 
-                    }
+                                          
+                if (params.HERRAMIENTA == 'gradle'){
+                    	//def ejecucion = load 'gradle.groovy'
+	                    //ejecucion.call()
+                        gradle.call(stage);
+                } else {
+                    	//def ejecucion = load 'maven.groovy'
+	                    //ejecucion.call()   
+                        maven.call();                 
+                }
+
                 }
             }
         }
+    }
 
-        post {
-            success{
-                //: [Nombre Alumno][Nombre Job][buildTool] Ejecución exitosa
-                slackSend color: 'good', message: "[Andres Aldana][${env.JOB_NAME}][${env.HERRAMIENTA}]Ejecucion exitosa"           
-            }
-
-            failure{
-                //[Nombre Alumno][Nombre Job][buildTool] Ejecución fallida en stage [Stage]
-                //la variable env.TAREA esta definida en los groovy
-                slackSend color: 'danger', message: "[Andres Aldana][${env.JOB_NAME}][${env.HERRAMIENTA}]Ejecución fallida en stage [${env.TAREA}]"                   
-            }
+    post {
+        success{
+            //: [Nombre Alumno][Nombre Job][buildTool] Ejecución exitosa
+            slackSend color: 'good', message: "[Rodrigo Zuniga][${env.JOB_NAME}][${env.HERRAMIENTA}]Ejecucion exitosa"           
         }
 
+        failure{
+            //[Nombre Alumno][Nombre Job][buildTool] Ejecución fallida en stage [Stage]
+            //la variable env.TAREA esta definida en los groovy
+            slackSend color: 'danger', message: "[Rodrigo Zuniga][${env.JOB_NAME}][${env.HERRAMIENTA}]Ejecución fallida en stage [${env.TAREA}]"                   
+        }
     }
+
 }
+
+
+}
+
 return this;
