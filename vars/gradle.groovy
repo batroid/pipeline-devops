4e878c2a816100d9a14a80a2f50cf64929111892
Andres Aldana <batroid@gmail.com>
2021-01-02 21:29:21 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -4,26 +4,18 @@ def call(stageOptions) {
 
 	if (stageOptions.contains('Build') || stageOptions.contains('Test') || stageOptions == ''){
 		stage('Build & Test') {
-	        	env.TASK = env.STAGE_NAME
         		sh './gradlew clean build'
     		}
 	}
     
 	if (stageOptions.contains('Sonar') || stageOptions == ''){
 		stage('Sonar') {
-        		env.TASK = env.STAGE_NAME
-		        //corresponde al scanner configurado en global tools Jenkins
-        		def scannerHome = tool 'sonar-scanner'
-        		//corresponde a lo configurado en sistema Jenkins
-        		withSonarQubeEnv('sonar-server') {
-            			bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
-        		}
+		      	echo 'ejecutando sonar'
         }
     }
 		
 	if (stageOptions.contains('Run') || stageOptions == ''){
 		stage('Run') {
-        		env.TASK = env.STAGE_NAME
         		sh './gradlew bootRun &'
         		sh 'sleep 20'
     		}
@@ -31,15 +23,13 @@ def call(stageOptions) {
 		
 	if (stageOptions.contains('Rest') || stageOptions == ''){
  		stage('Rest') {
-        		env.TASK = env.STAGE_NAME
         		sh 'curl -X GET "http://localhost:8888/rest/mscovid/test?msg=testing"'
     		}
 	}
 		
-	if (stageOptions.contains('Run') || stageOptions == ''){
+	if (stageOptions.contains('Nexus') || stageOptions == ''){
 		stage('Nexus') {
-        		env.TASK = env.STAGE_NAME
-		        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:/Proyectos/ejemplo-gradle/build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+		      	echo 'ejecutando Nexus'
     		}
 	}
 
