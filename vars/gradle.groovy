ca5037e809968487b9e51fe81e39d023a1a4c2b8
Andres Aldana <batroid@gmail.com>
2021-01-02 21:18:32 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -1,34 +1,50 @@
 /*
-	forma de invocación de método call:
-	def ejecucion = load 'script.groovy'
-	ejecucion.call()
+    forma de invocación de método call:
+    def ejecucion = load 'script.groovy'
+    ejecucion.call()
 */
 
-def call(){
-  
-        stage("Build & test"){   
-            env.TAREA =  env.STAGE_NAME        
-            sh "./gradlew clean build"            
-        }
-        stage("Sonar"){
-            env.TAREA =  env.STAGE_NAME 
-            def scannerHome = tool 'sonar-scanner';    
-            withSonarQubeEnv('sonar-server') { 
-                sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"   
-            }                        
-        }
-        stage("Run"){
-            env.TAREA =  env.STAGE_NAME 
-            sh "nohup bash gradlew bootRun &"
-            sleep 20                        
-        }
-        stage("Rest"){
-            env.TAREA =  env.STAGE_NAME 
-            sh 'curl -X GET "http://localhost:8081/rest/mscovid/test?msg=testing"'
-        }  
-        stage("Nexus"){    
-            env.TAREA =  env.STAGE_NAME        
-            nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '2.0.1']]]                     
-        }                 
+def call(stage) {
+
+	if (stage.contains('Build') || stage.contains('Test') || stage == ''){
+		stage('Build & Test') {
+	        	env.TASK = env.STAGE_NAME
+        		sh './gradlew clean build'
+    		}
+	}
+    
+	if (stage.contains('Sonar') || stage == ''){
+		stage('Sonar') {
+        		env.TASK = env.STAGE_NAME
+		        //corresponde al scanner configurado en global tools Jenkins
+        		def scannerHome = tool 'sonar-scanner'
+        		//corresponde a lo configurado en sistema Jenkins
+        		withSonarQubeEnv('sonar-server') {
+            			bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+        	}
+    	}
+		
+	if (stage.contains('Run') || stage == ''){
+		stage('Run') {
+        		env.TASK = env.STAGE_NAME
+        		sh './gradlew bootRun &'
+        		sh 'sleep 20'
+    		}
+	}
+		
+	if (stage.contains('Run') || stage == ''){
+ 		stage('Run') {
+        		env.TASK = env.STAGE_NAME
+        		sh 'curl -X GET "http://localhost:8888/rest/mscovid/test?msg=testing"'
+    		}
+	}
+		
+	if (stage.contains('Run') || stage == ''){
+		stage('Nexus') {
+        		env.TASK = env.STAGE_NAME
+		        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:/Proyectos/ejemplo-gradle/build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+    		}
+	}
 
 }
+return this;
